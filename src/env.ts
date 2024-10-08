4d12d800038898af9fabc60c25ad51c3b2f87181
GodaHaruki <haru.11.saki.11@gmail.com>
2024-10-09 03:29:03 +0900

    add USE_REPLYTOKEN option

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,8 @@
+// all of them are required
+
 export const CHANNEL_ACCESS_TOKEN: string = process.env.CHANNEL_ACCESS_TOKEN!; // "00000"
 export const IDS: string[] = JSON.parse(process.env.IDS!); // ["000","111"]
 export const LOG_SHEET_ID: string = process.env.LOG_SHEET_ID!;
 export const LOG_SHEET_NAME: string = process.env.LOG_SHEET_NAME!;
+export const USE_REPLYTOKEN: boolean = parseInt(process.env.USE_REPLYTOKEN!) ? true : false // 1 or 0
+export const REPLYTOKEN_SERVER_URL: string = process.env.REPLYTOKEN_SERVER_URL!; // https://github.com/GodaHaruki/Line-replytoken-bot
--- src/webhookActionHandlers.ts
+++ src/webhookActionHandlers.ts
@@ -1,17 +1,30 @@
 import { PullRequestClosedEvent, PullRequestOpenedEvent } from "@octokit/webhooks-types";
-import { IDS } from "./env";
-import { push } from "./line";
+import { IDS, REPLYTOKEN_SERVER_URL, USE_REPLYTOKEN } from "./env";
+import { push, reply } from "./line";
+import { Message } from "@line/bot-sdk";
 
 export function onPullRequestOpened(evt: PullRequestOpenedEvent) {
     IDS.forEach(id => {
-        push(id, [{
+        const msgs: Message[] = [{
             type: "text",
             text:
                 `Pull Request Created
 on ${evt.pull_request.created_at}
 by ${evt.pull_request.user.login}
-${evt.pull_request.html_url}
-`
-        }])
+${evt.pull_request.html_url}`
+        }];
+
+        if (USE_REPLYTOKEN) {
+            const replyTokens: {
+                "timestamp": number
+                "id": string
+                "replyToken": string // https://github.com/GodaHaruki/Line-replytoken-bot
+            }[] = JSON.parse(UrlFetchApp.fetch(REPLYTOKEN_SERVER_URL + "?id=" + id).getContentText());
+
+            if (replyTokens.length > 0) reply(replyTokens[0].replyToken, msgs);
+            else push(id, msgs);
+        } else {
+            push(id, msgs)
+        }
     })
 }
