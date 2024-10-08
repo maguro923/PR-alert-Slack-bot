cda98504dd87368021b079307e4ae4e13a2e1b4c
GodaHaruki <haru.11.saki.11@gmail.com>
2024-10-09 03:39:57 +0900

    fix bug

--- src/log.ts
+++ src/log.ts
@@ -4,5 +4,5 @@ export function log(l: string) {
     const sheet = SpreadsheetApp.openById(LOG_SHEET_ID).getSheetByName(LOG_SHEET_NAME)!;
     const lastRow = sheet.getLastRow();
     const timestamp = (new Date).getTime();
-    sheet.getRange(lastRow + 1, 1, 1, 2).setValues([[timestamp, log]])
+    sheet.getRange(lastRow + 1, 1, 1, 2).setValues([[timestamp, l]])
 }
