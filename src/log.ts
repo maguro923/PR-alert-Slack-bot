205148bc4d1ff63ded36adb440a7ae4745433774
GodaHaruki <haru.11.saki.11@gmail.com>
2024-10-09 02:47:28 +0900

    add env LOG_SHEET_ID, LOG_SHEET_NAME

--- src/log.ts
+++ src/log.ts
@@ -1,5 +1,7 @@
-export function log(l: string){
-    const sheet = SpreadsheetApp.getActiveSheet();
+import { LOG_SHEET_ID, LOG_SHEET_NAME } from "./env";
+
+export function log(l: string) {
+    const sheet = SpreadsheetApp.openById(LOG_SHEET_ID).getSheetByName(LOG_SHEET_NAME)!;
     const lastRow = sheet.getLastRow();
     const timestamp = (new Date).getTime();
     sheet.getRange(lastRow + 1, 1, 1, 2).setValues([[timestamp, log]])
