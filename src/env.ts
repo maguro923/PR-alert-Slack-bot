469b9fa3cac00472c85cf54c5389182ced6f4e00
GodaHaruki <haru.11.saki.11@gmail.com>
2024-10-09 02:54:14 +0900

    change variable name

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const CHANNEL_ACCESS_TOKEN: string = process.env.CHANNEL_ACCESS_TOKEN!; // "00000"
-export const USER_IDS: string[] = JSON.parse(process.env.USER_IDS!); // ["000","111"]
+export const IDS: string[] = JSON.parse(process.env.IDS!); // ["000","111"]
 export const LOG_SHEET_ID: string = process.env.LOG_SHEET_ID!;
 export const LOG_SHEET_NAME: string = process.env.LOG_SHEET_NAME!;
--- src/webhookActionHandlers.ts
+++ src/webhookActionHandlers.ts
@@ -1,13 +1,13 @@
 import { PullRequestOpenedEvent } from "@octokit/webhooks-types";
-import { USER_IDS } from "./env";
+import { IDS } from "./env";
 import { push } from "./line";
 
-export function onPullRequestOpened(evt: PullRequestOpenedEvent){
-    USER_IDS.forEach(id => {
+export function onPullRequestOpened(evt: PullRequestOpenedEvent) {
+    IDS.forEach(id => {
         push(id, [{
             type: "text",
-            text: 
-`Pull Request Created
+            text:
+                `Pull Request Created
 on ${evt.pull_request.created_at}
 by ${evt.pull_request.user.name}
 ${evt.pull_request.url}
