4a0be3bb10d453ff1a5fd17446b3350aa07d084c
GodaHaruki <haru.11.saki.11@gmail.com>
2024-10-09 02:46:43 +0900

    fix layout

--- src/doPost.ts
+++ src/doPost.ts
@@ -4,7 +4,7 @@ import { onPullRequestOpened } from "./webhookActionHandlers";
 export function doPost(evt: GoogleAppsScript.Events.DoPost) {
   const prEvt: PullRequestEvent = JSON.parse(evt.postData.contents);
 
-  switch (prEvt.action){
+  switch (prEvt.action) {
     case "opened":
       onPullRequestOpened(prEvt);
       break;
