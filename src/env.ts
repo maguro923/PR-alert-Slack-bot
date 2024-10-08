f9e72a591fd72fbe509ec0856e6213596f4caa97
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2024-10-08 04:18:42 +0000

    impl Line Messaging API

---
+++ src/env.ts
@@ -0,0 +1,2 @@
+export const CHANNEL_ACCESS_TOKEN: string = "";
+export const USER_IDS: string[] = []
---
+++ src/line.ts
@@ -0,0 +1,34 @@
+import { Message } from "@line/bot-sdk";
+import { CHANNEL_ACCESS_TOKEN } from "./env";
+
+export function send(to: string, msgs: Message[]){
+    const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+        contentType: "application/json",
+        method: "post",
+        headers: {
+            Authorization: "Bearer " + CHANNEL_ACCESS_TOKEN
+        },
+        payload: JSON.stringify({
+            to: to,
+            messages: msgs
+        }),
+    };
+
+    UrlFetchApp.fetch("https://api.line.me/v2/bot/message/push", options);
+}
+
+export function reply(replyToken: string, msgs: Message[]){
+    const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+        contentType: "application/json",
+        method: "post",
+        headers: {
+            Authorization: "Bearer " + CHANNEL_ACCESS_TOKEN,
+        },
+        payload: JSON.stringify({
+            replyToken: replyToken,
+            messages: msgs
+        })
+    };
+
+    UrlFetchApp.fetch("https://api.line.me/v2/bot/message/reply", options);
+}
